b49fe2b5c1a193b35dba480260476959dd514b72
Taras Zhurenko <zhta21@yandex.ru>
2021-12-16 00:27:45 +0300

    Update2 SQRServiceTest

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,10 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void shouldLimitedSquaring() {
+    void shouldIncludesPartValues() {
         SQRService service = new SQRService();
         int expected = 3;
         int actual = service.limitedSquaring(200,300);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldIncludedAllValues() {
+        SQRService service = new SQRService();
+        int expected = 90;
+        int actual = service.limitedSquaring(100,9801);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldNotIncludeValues() {
+        SQRService service = new SQRService();
+        int expected = 0;
+        int actual = service.limitedSquaring(10,90);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldBeyondValues() {
+        SQRService service = new SQRService();
+        int expected = 90;
+        int actual = service.limitedSquaring(0,10_000);
         assertEquals(expected,actual);
     }
 }
