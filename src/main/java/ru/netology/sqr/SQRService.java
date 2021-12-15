932bbfcde4887313ada0d40243ec93eda0b80345
Taras Zhurenko <zhta21@yandex.ru>
2021-12-16 00:54:59 +0300

    Update3 SQRService

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,7 +5,7 @@ public class SQRService {
         int output = 0;
         for (int i = 10; i >= 10 && i <= 99; i++) {
             if (i * i >= lowerLimit && i * i <= upperLimit) {
-                output = output + 1;
+                output++;
             }
         }
         return output;
