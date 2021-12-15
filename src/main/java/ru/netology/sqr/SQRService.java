245108a5b611e1b8c094e399b2244b03b095b1d6
Taras Zhurenko <zhta21@yandex.ru>
2021-12-15 22:47:56 +0300

    Update1 SQRService

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,4 +1,13 @@
 package ru.netology.sqr;
 
 public class SQRService {
+    public int limitedSquaring(int number) {
+        int output = 0;
+        for (int i = number; i >= 10 && i <= 99; i++) {
+            if (i * i >= 200 && i * i <= 300) {
+                output = output + 1;
+            }
+        }
+        return output;
+    }
 }
