58af9c0740a68afac263cc8dfabe9fb9290d4e44
Taras Zhurenko <zhta21@yandex.ru>
2021-12-15 22:48:43 +0300

    Add SQRServiceTest

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @Test
+    void limitedSquaring() {
+    }
+}
